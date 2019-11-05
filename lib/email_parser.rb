5d07349997f71d83d0dbe1271784ff373071a12b
Hans Foster <fosterhans@gmail.com>
2019-11-05 22:25:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,9 +12,9 @@ class EmailAddressParser
   
   def parse
     new_email_list = []
-    new_email_list = @emails.split(" ")
+    new_email_list = @emails.split(", ")
     if new_email_list.size == 1 
-      new_email_list = @emails.split(", ")
+      new_email_list = @emails.split(" ")
     end
   
     new_email_list = new_email_list.uniq
